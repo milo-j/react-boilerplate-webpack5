92a185127f70f96f7b7c80e05e0cfceda37f1ce1
milo-j <search2015@126.com>
2024-11-28 16:50:02 +0800

    first commit

---
+++ server/middlewares/addDevMiddlewares.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+function createWebpackMiddleware(compiler, publicPath) {
+  return webpackDevMiddleware(compiler, {
+    publicPath,
+    stats: {
+      all: false, // 禁用所有日志
+      errors: true, // 只显示错误信息
+      warnings: false, // 禁用警告信息
+    },
+  });
+}
+
+module.exports = function addDevMiddlewares(app, webpackConfig) {
+  const compiler = webpack(webpackConfig);
+  const middleware = createWebpackMiddleware(
+    compiler,
+    webpackConfig.output.publicPath,
+  );
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+
+  // Since webpackDevMiddleware uses memory-fs internally to store build
+  // artifacts, we use it instead
+  // const fs = middleware.fileSystem;
+  const fs = middleware.context.outputFileSystem;
+
+  app.get('*', (req, res) => {
+    fs.readFile(path.join(compiler.outputPath, 'index.html'), (err, file) => {
+      if (err) {
+        res.sendStatus(404);
+      } else {
+        res.send(file.toString());
+      }
+    });
+  });
+};
